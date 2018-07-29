f719c4ffe1498c28bb8d9eabdc09684fa9e152a2
halfstring <i@halfstring.com>
2018-07-29 16:25:21 +0800

    init

--- test.go
+++ test.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-	ecache "./src"
 	"time"
-	"fmt"
 	"log"
+	"fmt"
+	ecache "./src"
 )
 
 type myData struct {
@@ -12,11 +12,13 @@ type myData struct {
 }
 
 func main() {
+
+	//假如 想根据map的key来动态实例化的结构体,然后修改结构体的值，比如:    //obj := m[1]    //obj.One = 10
 	cache := ecache.Cache("default")
 
 	cache.Add("kalep", &myData{value: "kalepvalue"}, 50*time.Second)
 
-	v,ok:= cache.Value("kalep")
+	v, ok := cache.Value("kalep")
 	if ok != nil {
 		log.Println(ok)
 	}
